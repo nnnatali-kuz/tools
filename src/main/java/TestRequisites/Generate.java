b9f475ede73726697e7b03c78197d30e545f0532
Natalia Kuznetsova <nnnatus@gmail.com>
2018-08-07 14:57:52 +0300

    Добавление случайного выбора строки

--- src/main/java/TestRequisites/Generate.java
+++ src/main/java/TestRequisites/Generate.java
@@ -2,14 +2,22 @@ package TestRequisites;
 
 
 import java.io.IOException;
+import java.util.Random;
 
 public class Generate {
     public static void main(String[] args)
             throws IOException
     {
-       Requisites ur_rus_ooo = new Requisites("src\\data\\UR_RUS_OOO.txt");
+        final Random r = new Random();
 
-       Firm one = new Firm(ur_rus_ooo.getFromFile(0).split("\t"));
-       System.out.println(one.getKs());
+        Requisites ur_rus_ooo = new Requisites("src\\data\\UR_RUS_OOO.txt");
+
+        Firm one = new Firm(ur_rus_ooo.getFromFile(r.nextInt(1000)).split("\t"));
+        System.out.println(one.getInn());
+        System.out.println(one.getOgrn());
+        System.out.println(one.getKpp());
+        System.out.println(one.getRs());
+        System.out.println(one.getBik());
+        System.out.println(one.getKs());
     }
 }
