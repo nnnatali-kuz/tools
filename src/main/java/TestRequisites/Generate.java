08f3d0c214dcdff522f2a686f9cf00b9f5d8f15c
Natalia Kuznetsova <nnnatus@gmail.com>
2018-08-08 16:33:02 +0300

    Добавлена возможность выбора типа реквизитов вручную

--- src/main/java/TestRequisites/Generate.java
+++ src/main/java/TestRequisites/Generate.java
@@ -8,11 +8,64 @@ public class Generate {
     public static void main(String[] args)
             throws IOException
     {
+        int choice=1;
+        String path;
         final Random r = new Random();
 
-        Requisites ur_rus_ooo = new Requisites("src\\data\\UR_RUS_OOO.txt");
+        switch (choice) {
+            case 0:
+                path = "src\\data\\UR_RUS.txt"; //Российские реквизиты для юр. лиц (ООО)
+                break;
+            case 1:
+                path = "src\\data\\IP_RUS.txt"; //Российские реквизиты для ИП
+                break;
+            case 2:
+                path = "src\\data\\F_RUS.txt"; //Российские реквизиты для физ. лиц
+                break;
+            case 3:
+                path = "src\\data\\UR_UKR.txt"; //Украинские реквизиты для юр. лиц
+                break;
+            case 4:
+                path = "src\\data\\IP_UKR.txt"; //Украинские реквизиты для ИП
+                break;
+            case 5:
+                path = "src\\data\\F_UKR.txt"; //Украинские реквизиты для физ. лиц
+                break;
+            case 6:
+                path = "src\\data\\UR_KZT.txt"; //Казахские реквизиты для юр. лиц
+                break;
+            case 7:
+                path = "src\\data\\IP_KZT.txt"; //Казахские реквизиты для ИП
+                break;
+            case 8:
+                path = "src\\data\\F_KZT.txt"; //Казахские реквизиты для физ. лиц
+                break;
+            case 9:
+                path = "src\\data\\UR_BEL.txt"; //Белорусские реквизиты для юр. лиц
+                break;
+            case 10:
+                path = "src\\data\\IP_BEL.txt"; //Белорусские реквизиты для ИП
+                break;
+            case 11:
+                path = "src\\data\\F_BEL.txt"; //Белорусские реквизиты для физ. лиц
+                break;
+            case 12:
+                path = "src\\data\\UR_INT.txt"; //Международные реквизиты для юр. лиц
+                break;
+            case 13:
+                path = "src\\data\\IP_INT.txt"; //Международные реквизиты для ИП
+                break;
+            case 14:
+                path = "src\\data\\F_INT.txt"; //Международные реквизиты для физ. лиц
+                break;
+            default:
+                path = "src\\data\\UR_RUS.txt"; //Российские реквизиты для юр. лиц (ООО)
+                break;
+        }
 
-        Firm one = new Firm(ur_rus_ooo.getFromFile(r.nextInt(1000)).split("\t"));
+        Requisites file = new Requisites(path);
+        Firm one = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+
         System.out.println("Название:\t" + one.getName());
         System.out.println("ИНН:\t\t" + one.getInn());
         System.out.println("ОГРН:\t\t" + one.getOgrn());
