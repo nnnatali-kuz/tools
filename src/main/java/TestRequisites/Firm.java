729c6dade38e1a297d3a7bab89f57a211d4fa2d9
Natalia Kuznetsova <nnnatus@gmail.com>
2018-08-09 14:44:40 +0300

    Добавлено поле телефон в таблицы, реализован вывод реквизитов в зависимости от типа (только необходимых)

--- src/main/java/TestRequisites/Firm.java
+++ src/main/java/TestRequisites/Firm.java
@@ -1,24 +1,28 @@
 package TestRequisites;
 
 public class Firm {
-    private String name,inn, ogrn, kpp, rs, bik, bank, ks, postindex, city, street, house;
+    private String name,phone, inn, ogrn, kpp, rs, bik, bank, ks, postindex, city, street, house;
 
     Firm(String [] req){
         name = req [0];
-        inn = req[1];
-        ogrn = req [2];
-        kpp = req [3];
-        rs = req [4];
-        bik = req [5];
-        bank = req [6];
-        ks = req [7];
-        postindex = req [8];
-        city = req [9];
-        street = req [10];
-        house = req [11];
+        phone = req [1];
+        inn = req[2];
+        ogrn = req [3];
+        kpp = req [4];
+        rs = req [5];
+        bik = req [6];
+        bank = req [7];
+        ks = req [8];
+        postindex = req [9];
+        city = req [10];
+        street = req [11];
+        house = req [12];
     }
     public String getName(){
         return name;
+    }
+    public String getPhone(){
+        return phone;
     }
     public String getInn(){
         return inn;
@@ -53,4 +57,103 @@ public class Firm {
     public String getHouse() {
         return house;
     }
+
+    public void urRus(){
+        System.out.println("Название:\t" + name);
+        System.out.println("ИНН:\t\t" + inn);
+        System.out.println("ОГРН:\t\t" + ogrn);
+        System.out.println("КПП:\t\t" + kpp);
+        System.out.println("Р/с:\t\t" + rs);
+        System.out.println("БИК:\t\t" + bik);
+        System.out.println("Банк:\t\t" + bank);
+        System.out.println("К/с:\t\t" + ks);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void ipRus(){
+        System.out.println("Название:\t" + name);
+        System.out.println("ИНН:\t\t" + inn);
+        System.out.println("ОГРНИП:\t\t" + ogrn);
+        System.out.println("Р/с:\t\t" + rs);
+        System.out.println("БИК:\t\t" + bik);
+        System.out.println("Банк:\t\t" + bank);
+        System.out.println("К/с:\t\t" + ks);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void fRusUkr() {
+        System.out.println("ФИО:\t\t" + name);
+        System.out.println("Телефон:\t\t" + phone);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void urUkr(){
+        System.out.println("Название:\t" + name);
+        System.out.println("ЕДРПОУ:\t\t" + inn);
+        System.out.println("Р/с:\t\t" + rs);
+        System.out.println("МФО:\t\t" + bik);
+        System.out.println("Банк:\t\t" + bank);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void urKzt(){
+        System.out.println("Название:\t" + name);
+        System.out.println("БИН:\t\t" + inn);
+        System.out.println("ИИК:\t\t" + rs);
+        System.out.println("БИК:\t\t" + bik);
+        System.out.println("Банк:\t\t" + bank);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void ipKzt(){
+        System.out.println("Название:\t" + name);
+        System.out.println("ИИН:\t\t" + inn);
+        System.out.println("ИИК:\t\t" + rs);
+        System.out.println("БИК:\t\t" + bik);
+        System.out.println("Банк:\t\t" + bank);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void fKzt(){
+        System.out.println("ФИО:\t\t" + name);
+        System.out.println("Телефон:\t\t" + phone);
+        System.out.println("ИИН:\t\t" + inn);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void urBel() {
+        System.out.println("Название:\t" + name);
+        System.out.println("УНП:\t\t" + inn);
+        System.out.println("Р/с:\t\t" + rs);
+        System.out.println("Код:\t\t" + bik);
+        System.out.println("Банк:\t\t" + bank);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
+    public void fBel() {
+        System.out.println("ФИО:\t\t" + name);
+        System.out.println("Телефон:\t\t" + phone);
+        System.out.println("УНП:\t\t" + inn);
+        System.out.println("Индекс:\t\t" + postindex);
+        System.out.println("Нас. п.:\t" + city);
+        System.out.println("Улица:\t\t" + street);
+        System.out.println("Дом:\t\t" + house);
+    }
 }
+
--- src/main/java/TestRequisites/Generate.java
+++ src/main/java/TestRequisites/Generate.java
@@ -8,48 +8,84 @@ public class Generate {
     public static void main(String[] args)
             throws IOException
     {
-        int choice=1;
-        String path;
+        int choice=0;
         final Random r = new Random();
 
-        switch (choice) {
-            case 0:
-                path = "src\\data\\UR_RUS.txt"; //Российские реквизиты для юр. лиц (ООО)
+        switch (choice)
+        {
+            case 0: {
+                Requisites file = new Requisites("src\\data\\UR_RUS.txt"); //Российские реквизиты для юр. лиц (ООО)
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.urRus();
                 break;
-            case 1:
-                path = "src\\data\\IP_RUS.txt"; //Российские реквизиты для ИП
+            }
+            case 1: {
+                Requisites file = new Requisites("src\\data\\IP_RUS.txt"); //Российские реквизиты для ИП
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.ipRus();
                 break;
-            case 2:
-                path = "src\\data\\F_RUS.txt"; //Российские реквизиты для физ. лиц
+            }
+            case 2: {
+                Requisites file = new Requisites("src\\data\\F_RUS.txt"); //Российские реквизиты для физ. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.fRusUkr();
                 break;
-            case 3:
-                path = "src\\data\\UR_UKR.txt"; //Украинские реквизиты для юр. лиц
+            }
+            case 3: {
+                Requisites file = new Requisites("src\\data\\UR_UKR.txt"); //Украинские реквизиты для юр. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.urUkr();
                 break;
-            case 4:
-                path = "src\\data\\IP_UKR.txt"; //Украинские реквизиты для ИП
+            }
+            case 4: {
+                Requisites file = new Requisites("src\\data\\IP_UKR.txt"); //Украинские реквизиты для ИП
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.urUkr();
                 break;
-            case 5:
-                path = "src\\data\\F_UKR.txt"; //Украинские реквизиты для физ. лиц
+            }
+            case 5: {
+                Requisites file = new Requisites("src\\data\\F_UKR.txt"); //Украинские реквизиты для физ. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.fRusUkr();
                 break;
-            case 6:
-                path = "src\\data\\UR_KZT.txt"; //Казахские реквизиты для юр. лиц
+            }
+            case 6: {
+                Requisites file = new Requisites("src\\data\\UR_KZT.txt"); //Казахские реквизиты для юр. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.urKzt();
                 break;
-            case 7:
-                path = "src\\data\\IP_KZT.txt"; //Казахские реквизиты для ИП
+            }
+            case 7: {
+                Requisites file = new Requisites("src\\data\\IP_KZT.txt"); //Казахские реквизиты для ИП
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.ipKzt();
                 break;
-            case 8:
-                path = "src\\data\\F_KZT.txt"; //Казахские реквизиты для физ. лиц
+            }
+            case 8: {
+                Requisites file = new Requisites("src\\data\\F_KZT.txt"); //Казахские реквизиты для физ. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.fKzt();
                 break;
-            case 9:
-                path = "src\\data\\UR_BEL.txt"; //Белорусские реквизиты для юр. лиц
+            }
+            case 9: {
+                Requisites file = new Requisites("src\\data\\UR_BEL.txt"); //Белорусские реквизиты для юр. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.urBel();
                 break;
-            case 10:
-                path = "src\\data\\IP_BEL.txt"; //Белорусские реквизиты для ИП
+            }
+            case 10: {
+                Requisites file = new Requisites("src\\data\\IP_BEL.txt"); //Белорусские реквизиты для ИП
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.urBel();
                 break;
-            case 11:
-                path = "src\\data\\F_BEL.txt"; //Белорусские реквизиты для физ. лиц
+            }
+            case 11: {
+                Requisites file = new Requisites("src\\data\\F_BEL.txt"); //Белорусские реквизиты для физ. лиц
+                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                user.fBel();
                 break;
-            case 12:
+            }
+            /*case 12:
                 path = "src\\data\\UR_INT.txt"; //Международные реквизиты для юр. лиц
                 break;
             case 13:
@@ -57,26 +93,7 @@ public class Generate {
                 break;
             case 14:
                 path = "src\\data\\F_INT.txt"; //Международные реквизиты для физ. лиц
-                break;
-            default:
-                path = "src\\data\\UR_RUS.txt"; //Российские реквизиты для юр. лиц (ООО)
-                break;
+                break;*/
         }
-
-        Requisites file = new Requisites(path);
-        Firm one = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
-
-        System.out.println("Название:\t" + one.getName());
-        System.out.println("ИНН:\t\t" + one.getInn());
-        System.out.println("ОГРН:\t\t" + one.getOgrn());
-        System.out.println("КПП:\t\t" + one.getKpp());
-        System.out.println("Р/с:\t\t" + one.getRs());
-        System.out.println("БИК:\t\t" + one.getBik());
-        System.out.println("Банк:\t\t" + one.getBank());
-        System.out.println("К/с:\t\t" + one.getKs());
-        System.out.println("Индекс:\t\t" + one.getPostindex());
-        System.out.println("Нас. п.:\t" + one.getCity());
-        System.out.println("Улица:\t\t" + one.getStreet());
-        System.out.println("Дом:\t\t" + one.getHouse());
     }
 }
