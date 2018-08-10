5d9ea982160d40c2aa9b7ec856788f4ac189ddbf
Natalia Kuznetsova <nnnatus@gmail.com>
2018-08-10 10:26:15 +0300

    Перекомпонован класс Firm

--- src/main/java/TestRequisites/Firm.java
+++ src/main/java/TestRequisites/Firm.java
@@ -1,63 +1,48 @@
 package TestRequisites;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class Firm {
+    private String filePath;
     private String name,phone, inn, ogrn, kpp, rs, bik, bank, ks, postindex, city, street, house;
 
-    Firm(String [] req){
-        name = req [0];
-        phone = req [1];
-        inn = req[2];
-        ogrn = req [3];
-        kpp = req [4];
-        rs = req [5];
-        bik = req [6];
-        bank = req [7];
-        ks = req [8];
-        postindex = req [9];
-        city = req [10];
-        street = req [11];
-        house = req [12];
-    }
-    public String getName(){
-        return name;
-    }
-    public String getPhone(){
-        return phone;
-    }
-    public String getInn(){
-        return inn;
-    }
-    public String getOgrn(){
-        return ogrn;
-    }
-    public String getKpp(){
-        return kpp;
-    }
-    public String getRs(){
-        return rs;
-    }
-    public String getBik(){
-        return bik;
-    }
-    public String getBank(){
-        return bank;
-    }
-    public String getKs(){
-        return ks;
-    }
-    public String getPostindex(){
-        return postindex;
-    }
-    public String getCity() {
-        return city;
-    }
-    public String getStreet() {
-        return street;
-    }
-    public String getHouse() {
-        return house;
-    }
+    Firm(String p)
+    throws IOException {
 
+        int i = 0;
+        String buffer;
+        String[] requisite = new String[100];
+
+        filePath = p;
+
+        BufferedReader file = new BufferedReader(new FileReader(filePath));
+
+        while((buffer = file.readLine())!= null){
+            requisite[i] = buffer;
+            i++;
+        }
+        file.close();
+
+        final Random r = new Random();
+        int n = r.nextInt((requisite.length-1));
+
+        name = requisite[n].split("\t")[0];
+        phone = requisite[n].split("\t") [1];
+        inn = requisite[n].split("\t")[2];
+        ogrn = requisite[n].split("\t") [3];
+        kpp = requisite[n].split("\t") [4];
+        rs = requisite[n].split("\t") [5];
+        bik = requisite[n].split("\t") [6];
+        bank = requisite[n].split("\t") [7];
+        ks = requisite[n].split("\t") [8];
+        postindex = requisite[n].split("\t") [9];
+        city = requisite[n].split("\t") [10];
+        street = requisite[n].split("\t") [11];
+        house = requisite[n].split("\t") [12];
+    }
     public void urRus(){
         System.out.println("Название:\t" + name);
         System.out.println("ИНН:\t\t" + inn);
@@ -155,5 +140,46 @@ public class Firm {
         System.out.println("Улица:\t\t" + street);
         System.out.println("Дом:\t\t" + house);
     }
+
+
+    /*public String getName(){
+        return name;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public String getInn(){
+        return inn;
+    }
+    public String getOgrn(){
+        return ogrn;
+    }
+    public String getKpp(){
+        return kpp;
+    }
+    public String getRs(){
+        return rs;
+    }
+    public String getBik(){
+        return bik;
+    }
+    public String getBank(){
+        return bank;
+    }
+    public String getKs(){
+        return ks;
+    }
+    public String getPostindex(){
+        return postindex;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public String getHouse() {
+        return house;
+    }*/
 }
 
--- src/main/java/TestRequisites/Generate.java
+++ src/main/java/TestRequisites/Generate.java
@@ -2,86 +2,72 @@ package TestRequisites;
 
 
 import java.io.IOException;
-import java.util.Random;
 
 public class Generate {
     public static void main(String[] args)
             throws IOException
     {
         int choice=8;
-        final Random r = new Random();
 
         switch (choice)
         {
             case 0: {
-                Requisites file = new Requisites("src\\data\\UR_RUS.txt"); //Российские реквизиты для юр. лиц (ООО)
-                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                Firm user = new Firm("src\\data\\UR_RUS.txt"); //Российские реквизиты для юр. лиц (ООО)
                 user.urRus();
                 break;
             }
             case 1: {
-                Requisites file = new Requisites("src\\data\\IP_RUS.txt"); //Российские реквизиты для ИП
-                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                Firm user = new Firm("src\\data\\IP_RUS.txt"); //Российские реквизиты для ИП
                 user.ipRus();
                 break;
             }
             case 2: {
-                Requisites file = new Requisites("src\\data\\F_RUS.txt"); //Российские реквизиты для физ. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(1000)).split("\t"));
+                Firm user = new Firm("src\\data\\F_RUS.txt"); //Российские реквизиты для физ. лиц
                 user.fRusUkr();
                 break;
             }
             case 3: {
-                Requisites file = new Requisites("src\\data\\UR_UKR.txt"); //Украинские реквизиты для юр. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\UR_UKR.txt"); //Украинские реквизиты для юр. лиц
                 user.urUkr();
                 break;
             }
             case 4: {
-                Requisites file = new Requisites("src\\data\\IP_UKR.txt"); //Украинские реквизиты для ИП
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\IP_UKR.txt"); //Украинские реквизиты для ИП
                 user.urUkr();
                 break;
             }
             case 5: {
-                Requisites file = new Requisites("src\\data\\F_UKR.txt"); //Украинские реквизиты для физ. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\F_UKR.txt"); //Украинские реквизиты для физ. лиц
                 user.fRusUkr();
                 break;
             }
             case 6: {
-                Requisites file = new Requisites("src\\data\\UR_KZT.txt"); //Казахские реквизиты для юр. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\UR_KZT.txt"); //Казахские реквизиты для юр. лиц
                 user.urKzt();
                 break;
             }
             case 7: {
-                Requisites file = new Requisites("src\\data\\IP_KZT.txt"); //Казахские реквизиты для ИП
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\IP_KZT.txt"); //Казахские реквизиты для ИП
                 user.ipKzt();
                 break;
             }
             case 8: {
-                Requisites file = new Requisites("src\\data\\F_KZT.txt"); //Казахские реквизиты для физ. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\F_KZT.txt"); //Казахские реквизиты для физ. лиц
                 user.fKzt();
                 break;
             }
             case 9: {
-                Requisites file = new Requisites("src\\data\\UR_BEL.txt"); //Белорусские реквизиты для юр. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\UR_BEL.txt"); //Белорусские реквизиты для юр. лиц
                 user.urBel();
                 break;
             }
             case 10: {
-                Requisites file = new Requisites("src\\data\\IP_BEL.txt"); //Белорусские реквизиты для ИП
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\IP_BEL.txt"); //Белорусские реквизиты для ИП
                 user.urBel();
                 break;
             }
             case 11: {
-                Requisites file = new Requisites("src\\data\\F_BEL.txt"); //Белорусские реквизиты для физ. лиц
-                Firm user = new Firm(file.getFromFile(r.nextInt(100)).split("\t"));
+                Firm user = new Firm("src\\data\\F_BEL.txt"); //Белорусские реквизиты для физ. лиц
                 user.fBel();
                 break;
             }
